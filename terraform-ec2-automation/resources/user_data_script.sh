ce63bde42ec766e608dc87efc77169dc0ff3f3bf
jdfsa <jacques.sa@gmail.com>
2023-11-19 18:10:16 -0300

    feat: fix user_data script

--- terraform-ec2-automation/resources/user_data_script.sh
+++ terraform-ec2-automation/resources/user_data_script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo yum update -y
 
 # installing java 17
@@ -6,9 +8,11 @@ sudo yum install java-17-amazon-corretto -y
 
 # upgrading to python 3.8
 # ref: https://repost.aws/questions/QUtA3qNBaLSvWPfD5kFwI0_w/python-3-10-on-ec2-running-amazon-linux-2-and-the-openssl-upgrade-requirement
-sudo yum remove python3
+sudo yum remove python3 -y
 amazon-linux-extras | grep python
 sudo amazon-linux-extras install python3.8 -y
+sudo rm /usr/bin/python
+sudo ln -s /usr/bin/python3.8 /usr/bin/python
 
 # installing jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
